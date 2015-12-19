79caff3bfedcae5e50035468e03ee781ccb4874a
gdeioannes <Trendingames@Trendingames.local>
2015-12-19 16:00:23 -0300

    Chat Test With Jason

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
   socket.on('chat message', function(data){
       console.log(data);
-    //io.emit('chat message', msg);
+    io.emit('chat message', data);
   });
 });
 
