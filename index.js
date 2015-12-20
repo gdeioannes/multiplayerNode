5e657570de9944c305c1ed26d635f151d923151e
gdeioannes <Trendingames@Trendingames.local>
2015-12-19 23:51:43 -0300

    Server Test

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var receivedData;
+var players=[];
+var vel=10;
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
@@ -8,15 +11,49 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   socket.on('send dataChat', function(data){
-      console.log(data);
+    console.log(data);
+    setPlayers(data);
     io.emit('send dataChat', data);
   });
-  socket.on('send dataPlayer', function(data){
+    socket.on('send dataPlayer', function(data){
       console.log(data);
-    io.emit('send dataPlayer', data);
-  });
+    io.emit('send dataPlayer', players);
 });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
+///GAME
+
+function setPlayers(data){
+    players.push(data);
+}
+
+setInterval(mainLoop,30);
+
+function mainLoop(){
+  drawCircle(centerX,centerY,radius);  
+    for(var i=0;i<players.length;i++){
+      if(players[i]!=null){    
+      if(players[i].flagDown){
+          players[i].posy+=vel;
+      }
+      if(players[i].flagUp){ 
+          players[i].posy-=vel;
+      }
+      if(players[i].flagLeft){ 
+          players[i].posx-=vel;
+      }
+      if(players[i].flagRight){ 
+          players[i].posx=vel;
+      }  
+    }
+    }
+    
+    io.on('connection', function(socket){
+        io.emit('send dataPlayer', players);
+    });
+
+  });
+}
