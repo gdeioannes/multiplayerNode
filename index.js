62c6fa228f9528e1e4d2db55cf223dd80faba06c
gdeioannes <Trendingames@Trendingames.local>
2015-12-20 00:16:11 -0300

    Object fix

--- index.js
+++ index.js
@@ -28,9 +28,7 @@ http.listen(3000, function(){
 });
 
 function setPlayersData(data){
-    if(players[data.id]==null){
-        players[data.id][data];
-    }
+    
     console.log("Data:"+data);
     //console.log("Players:"+players);
 }
