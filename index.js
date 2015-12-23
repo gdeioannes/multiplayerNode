bbc9d12644f2b6360c5450bab698e8a644bf32a7
gdeioannes <Trendingames@Trendingames.local>
2015-12-23 01:41:21 -0300

    Radius Size

--- index.js
+++ index.js
@@ -177,7 +177,7 @@ function mainLoop(){
         }
     }
     for(var iii=0;iii<ligthPoints.length;iii++){
-        if(lineDistance({"x":playersServer[i].posx,"y":playersServer[i].posy},{"x":ligthPoints[iii].posx,"y":ligthPoints[iii].posy})-minRadius<radius){
+        if(lineDistance({"x":playersServer[i].posx,"y":playersServer[i].posy},{"x":ligthPoints[iii].posx,"y":ligthPoints[iii].posy})-minRadius<minRadius){
             
             playersServer[i].maxShootRadius+=ligthPoints[iii].radius;
             ligthPoints.splice(iii,1);
