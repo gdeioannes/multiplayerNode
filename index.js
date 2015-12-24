2560e174ecca0fcfd9b76a4de3ce05ead0f2f2db
gdeioannes <Trendingames@Trendingames.local>
2015-12-24 03:42:28 -0300

    test 2

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ function setLigthPointData(){
 
 function createLigthsPoints(){
     //console.log("Create Ligth Point");
-    for(var a=0;a<1000;a++){
+    for(var a=0;a<10000;a++){
         //console.log("Create Ligth Point " +a);
         setLigthPointData();
         
