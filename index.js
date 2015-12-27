f64739d29aa683c0373241fe66270f557d20233b
gdeioannes <Trendingames@Trendingames.local>
2015-12-27 16:11:45 -0300

    Colision energy in charge Radius

--- index.js
+++ index.js
@@ -263,7 +263,7 @@ function mainLoop(){
 
     //LIGTH POINTS LOGIC
     for(var iii=0;iii<ligthPoints.length;iii++){
-        if(lineDistance({"x":playersServer[i].posx,"y":playersServer[i].posy},{"x":ligthPoints[iii].posx,"y":ligthPoints[iii].posy})-minRadius<minRadius){
+        if(lineDistance({"x":playersServer[i].posx,"y":playersServer[i].posy},{"x":ligthPoints[iii].posx,"y":ligthPoints[iii].posy})-minRadius<playersServer[i].chargeRadius){
             playersServer[i].chargeRadius++;
             ligthPoints.splice(iii,1);
             setLigthPointData();
